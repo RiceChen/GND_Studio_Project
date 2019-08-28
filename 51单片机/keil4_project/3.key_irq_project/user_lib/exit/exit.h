c6987e33a6112117f0a803f436eb9a12328002db
HokyGUAN <Hoky.GUAN@tymphany.com>
2019-08-28 14:45:04 +0800

    [C51]Add key_irq_project

---
+++ 51单片机/keil4_project/3.key_irq_project/user_lib/exit/exit.h
@@ -0,0 +1,14 @@
+/***************************************************
+ *         Author: Hoky GUAN                       *
+ *         Date  : 2019/7/25                       *
+ *         Last  : 2019/7/25                       *
+ *         Notes : exit.h                          *
+ **************************************************/
+#ifndef _EXIT_H_
+#define _EXIT_H_
+
+#include <reg52.h>
+
+void exit_enable(char enable);
+
+#endif
